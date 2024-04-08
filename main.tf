afa9b008c0e477b1a2fa6246cd1ad4834a90b41e
shafikovrr <shafikov.r.r@ya.ru>
2024-04-08 10:03:24 +0000

    commit

--- main.tf
+++ main.tf
@@ -1,123 +1,106 @@
-
 # Настройка дисков
-# диск для vm-1
 resource "yandex_compute_disk" "boot-disk-vm-1" {
-  name     = "disk-vm-1"
-  type     = "network-hdd"
-  zone     = "ru-central1-a"
-  size     = 10
-  image_id = var.image_id
+  name        = "disk-vm-1"
+  description = "диск для веб-сервера 1"
+  type        = "network-hdd"
+  zone        = var.zone_1
+  size        = 10
+  image_id    = var.image_id
 }
 
-# диск для vm-2
 resource "yandex_compute_disk" "boot-disk-vm-2" {
-  name     = "disk-vm-2"
-  type     = "network-hdd"
-  zone     = "ru-central1-b"
-  size     = 10
-  image_id = var.image_id
+  name        = "disk-vm-2"
+  description = "диск для веб-сервера 2"
+  type        = "network-hdd"
+  zone        = var.zone_2
+  size        = 10
+  image_id    = var.image_id
 }
 
 # Настройка хостов
-# host 1
+## Веб-сервера
 resource "yandex_compute_instance" "vm-1" {
   name        = "vm-1"
+  description = "веб-сервер 1"
   hostname    = "vm-1"
-  zone        = "ru-central1-a"
-  platform_id = "standard-v2"
-
+  zone        = var.zone_1
+  platform_id = var.platform
   resources {
     cores         = 2
     core_fraction = 20
     memory        = 2
   }
-
   boot_disk {
     disk_id = yandex_compute_disk.boot-disk-vm-1.id
   }
-
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
+    subnet_id = yandex_vpc_subnet.subnet-a.id
     nat       = true
   }
-
   metadata = {
     user-data = "${file("./meta.yml")}"
   }
-
   scheduling_policy {
     preemptible = true
   }
 }
 
-# host 2
 resource "yandex_compute_instance" "vm-2" {
   name        = "vm-2"
+  description = "веб-сервер 2"
   hostname    = "vm-2"
-  zone        = "ru-central1-b"
-  platform_id = "standard-v2"
-
+  zone        = var.zone_2
+  platform_id = var.platform
   resources {
     cores         = 2
     core_fraction = 20
     memory        = 2
   }
-
   boot_disk {
     disk_id = yandex_compute_disk.boot-disk-vm-2.id
   }
-
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-2.id
+    subnet_id = yandex_vpc_subnet.subnet-b.id
     nat       = true
   }
-
   metadata = {
     user-data = "${file("./meta.yml")}"
   }
-
   scheduling_policy {
     preemptible = true
   }
 }
 
-# настройка сети в разных зонах и подсетях https://yandex.cloud/ru/docs/vpc/operations/subnet-create
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
+######################################################################################################
+# настройка сети в разных зонах и подсетях https://yandex.cloud/ru/docs/vpc/operations/subnet-create #
+######################################################################################################
+resource "yandex_vpc_network" "network" {
+  name = "network"
 }
-
-resource "yandex_vpc_subnet" "subnet-1" {
+resource "yandex_vpc_subnet" "subnet-a" {
   name           = "subnet1"
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-1.id
+  network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-
-resource "yandex_vpc_network" "network-2" {
-  name = "network2"
-}
-
-resource "yandex_vpc_subnet" "subnet-2" {
+resource "yandex_vpc_subnet" "subnet-b" {
   name           = "subnet2"
   zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.network-2.id
+  network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["192.168.11.0/24"]
 }
-
-# Вывод полученных ip (внутренний и внешние)
-
+##############################################
+# Вывод полученных ip (внутренних и внешних) #
+##############################################
 output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
-
 output "internal_ip_address_vm_2" {
   value = yandex_compute_instance.vm-2.network_interface.0.ip_address
 }
-
 output "external_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
-
 output "external_ip_address_vm_2" {
   value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
 }
@@ -132,16 +115,15 @@ resource "yandex_alb_target_group" "nginx-group" {
   name = "nginx-group"
 
   target {
-    subnet_id  = yandex_vpc_subnet.subnet-1.id
+    subnet_id  = yandex_vpc_subnet.subnet-a.id
     ip_address = yandex_compute_instance.vm-1.network_interface.0.ip_address
   }
 
   target {
-    subnet_id  = yandex_vpc_subnet.subnet-2.id
+    subnet_id  = yandex_vpc_subnet.subnet-b.id
     ip_address = yandex_compute_instance.vm-2.network_interface.0.ip_address
   }
 }
-
 
 
 
