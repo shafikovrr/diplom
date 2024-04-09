e9663ac96c47df9586bee75ba02db3d6f4e6d782
shafikovrr <shafikov.r.r@ya.ru>
2024-04-09 04:40:05 +0000

    committed

--- main.tf
+++ main.tf
@@ -1,16 +1,15 @@
 #######
 # Настройка дисков
-resource "yandex_compute_disk" "boot-disk-vm-1" {
-  name        = "disk-vm-1"
+resource "yandex_compute_disk" "boot-disk-web-host-1" {
+  name        = "disk-web-host-1"
   description = "диск для веб-сервера 1"
   type        = "network-hdd"
   zone        = var.zone_a
   size        = 10
   image_id    = var.image_id
 }
-
-resource "yandex_compute_disk" "boot-disk-vm-2" {
-  name        = "disk-vm-2"
+resource "yandex_compute_disk" "boot-disk-web-host-2" {
+  name        = "disk-web-host-2"
   description = "диск для веб-сервера 2"
   type        = "network-hdd"
   zone        = var.zone_b
@@ -20,10 +19,10 @@ resource "yandex_compute_disk" "boot-disk-vm-2" {
 
 # Настройка хостов
 ## Веб-сервера
-resource "yandex_compute_instance" "vm-1" {
-  name        = "vm-1"
+resource "yandex_compute_instance" "web-host-1" {
+  name        = "web-host-1"
   description = "веб-сервер 1"
-  hostname    = "vm-1"
+  hostname    = "web-host-1"
   zone        = var.zone_a
   platform_id = var.platform
   resources {
@@ -32,7 +31,7 @@ resource "yandex_compute_instance" "vm-1" {
     memory        = 2
   }
   boot_disk {
-    disk_id = yandex_compute_disk.boot-disk-vm-1.id
+    disk_id = yandex_compute_disk.boot-disk-web-host-1.id
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.subnet-a.id
@@ -45,11 +44,10 @@ resource "yandex_compute_instance" "vm-1" {
     preemptible = true
   }
 }
-
-resource "yandex_compute_instance" "vm-2" {
-  name        = "vm-2"
+resource "yandex_compute_instance" "web-host-2" {
+  name        = "web-host-2"
   description = "веб-сервер 2"
-  hostname    = "vm-2"
+  hostname    = "web-host-2"
   zone        = var.zone_b
   platform_id = var.platform
   resources {
@@ -58,7 +56,7 @@ resource "yandex_compute_instance" "vm-2" {
     memory        = 2
   }
   boot_disk {
-    disk_id = yandex_compute_disk.boot-disk-vm-2.id
+    disk_id = yandex_compute_disk.boot-disk-web-host-2.id
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.subnet-b.id
@@ -94,50 +92,50 @@ resource "yandex_vpc_subnet" "subnet-b" {
 # Вывод полученных ip (внутренних и внешних) #
 ##############################################
 
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+output "internal_ip_address_web-host_1" {
+  value = yandex_compute_instance.web-host-1.network_interface.0.ip_address
 }
-output "internal_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+output "internal_ip_address_web-host_2" {
+  value = yandex_compute_instance.web-host-2.network_interface.0.ip_address
 }
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "external_ip_address_web-host_1" {
+  value = yandex_compute_instance.web-host-1.network_interface.0.nat_ip_address
 }
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+output "external_ip_address_web-host_2" {
+  value = yandex_compute_instance.web-host-2.network_interface.0.nat_ip_address
 }
-
 
 #################################################################################################
 # target-group https://yandex.cloud/ru/docs/application-load-balancer/operations/target-group-create#tf_1
 # посмотреть https://console.yandex.cloud/folders/b1gltt4aeqoofm7e2pnj/application-load-balancer/target-groups
-
-resource "yandex_alb_target_group" "nginx-group" {
-  name = "nginx-group"
+#######################################################################################################
+resource "yandex_alb_target_group" "web-hosts-group" {
+  name = "web-hosts-group"
   target {
     subnet_id  = yandex_vpc_subnet.subnet-a.id
-    ip_address = yandex_compute_instance.vm-1.network_interface.0.ip_address
+    ip_address = yandex_compute_instance.web-host-1.network_interface.0.ip_address
   }
   target {
     subnet_id  = yandex_vpc_subnet.subnet-b.id
-    ip_address = yandex_compute_instance.vm-2.network_interface.0.ip_address
+    ip_address = yandex_compute_instance.web-host-2.network_interface.0.ip_address
   }
 }
 
 ################################################################################################
 # https://yandex.cloud/ru/docs/application-load-balancer/operations/backend-group-create
+##################################################################################################
 resource "yandex_alb_backend_group" "backend-group" {
-  name = "web-hosts"
+  name = "web-hosts-backend-group"
   session_affinity {
     connection {
       source_ip = true
     }
   }
   http_backend {
-    name             = "web"
+    name             = "web-hosts-http-backend"
     weight           = 1
     port             = 80
-    target_group_ids = ["${yandex_alb_target_group.nginx-group.id}"]
+    target_group_ids = ["${yandex_alb_target_group.web-hosts-group.id}"]
     load_balancing_config {
       panic_threshold = 90
     }
@@ -153,16 +151,17 @@ resource "yandex_alb_backend_group" "backend-group" {
     }
   }
 }
-
+###################################################################################################
 # https://yandex.cloud/ru/docs/application-load-balancer/operations/http-router-create
+################################################################################################
 resource "yandex_alb_http_router" "tf-router" {
-  name = "wh-http-router"
+  name = "web-hosts-http-router"
 }
-resource "yandex_alb_virtual_host" "my-virtual-host" {
-  name           = "wh-virtual"
+resource "yandex_alb_virtual_host" "virtual-host" {
+  name           = "web-hosts-virtual-host"
   http_router_id = yandex_alb_http_router.tf-router.id
   route {
-    name = "wh-router"
+    name = "web-hosts-route"
     http_route { #как настроить /? https://terraform-provider.yandexcloud.net/Resources/alb_virtual_host / по умолчанию если не задано
       http_match {
         path {
