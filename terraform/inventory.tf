4d3cc2461fbee976c8a80b29da218cd8624dacf3
shafikovrr <shafikovrr@ya.ru>
2024-04-29 17:20:20 +0000

    a

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -2,40 +2,40 @@ resource "local_file" "inventory" {
   content  = <<EOF
 
 Host ${var.bastion_host_name}
-  HostName ${yandex_vpc_address.addr.external_ipv4_address.0.address}
+  HostName ${yandex_compute_instance.bastion.network_interface.0.nat_ip_address}
   User ${var.ssh_host_user}
   Port ${var.ssh_port}
   IdentityFile ${var.ssh_bastion}
 
-Host web_server_1.ru-central1.internal
+Host ${var.web1_host_name}
   HostName ${yandex_compute_instance.web-host-1.network_interface.0.ip_address}
   User ${var.ssh_host_user}
   Port ${var.ssh_port}
   IdentityFile ${var.ssh_bastion}
   ProxyJump ${var.bastion_host_name}
   
-Host web_server_2.ru-central1.internal
+Host ${var.web2_host_name}
   HostName ${yandex_compute_instance.web-host-2.network_interface.0.ip_address}
   User ${var.ssh_host_user}
   Port ${var.ssh_port}
   IdentityFile ${var.ssh_bastion}
   ProxyJump ${var.bastion_host_name}
 
-Host zabbix.ru-central1.internal
+Host ${var.zabbix_host_name}
   HostName ${yandex_compute_instance.zabbix.network_interface.0.ip_address}
   User ${var.ssh_host_user}
   Port ${var.ssh_port}
   IdentityFile ${var.ssh_bastion}
   ProxyJump ${var.bastion_host_name}
 
-Host kibana.ru-central1.internal
+Host ${var.kibana_host_name}
   HostName ${yandex_compute_instance.kibana.network_interface.0.ip_address}
   User ${var.ssh_host_user}
   Port ${var.ssh_port}
   IdentityFile ${var.ssh_bastion}
   ProxyJump ${var.bastion_host_name}
 
-Host elasticsearch.ru-central1.internal
+Host ${var.elasticsearch_host_name}
   HostName ${yandex_compute_instance.elasticsearch.network_interface.0.ip_address}
   User ${var.ssh_host_user}
   Port ${var.ssh_port}
